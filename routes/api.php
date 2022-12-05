251a992a03a3780ff2a9357ecbe08ee9950834a8
parthokar90 <parthokar90@gmail.com>
2022-12-05 12:03:09 +0600

    add migration file

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\user\AuthController;
 use App\Http\Controllers\user\DashboardController;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 
 /*
 |--------------------------------------------------------------------------
